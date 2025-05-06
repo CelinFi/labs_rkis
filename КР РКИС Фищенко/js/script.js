9c6a9d055948818c9d8f1e248166492469c58776
3-126-8-PC <you@example.com>
2025-05-06 17:12:37 +1200

    06.05.25-КР ркис

---
+++ КР РКИС Фищенко/js/script.js
@@ -0,0 +1,167 @@
+const host = "http://apiweb.api-web-tech.local"
+const content = document.querySelector(".content")
+var token = '';
+
+
+function _get (params, callback) {
+    let http_request = new XMLHttpRequest();
+    http_request.open('GET', `${params.url}`);
+    http_request.send();
+    http_request.onreadystatechange = function () {
+        if (http_request.readyState == 4) {
+            callback(http_request.response);
+        }
+    };
+}
+
+function _post (params, callback) {
+    let http_request = new XMLHttpRequest();
+    http_request.open('POST', `${params.url}`);
+    http_request.send(params.data);
+    http_request.onreadystatechange = function () {
+        if (http_request.readyState == 4){
+            callback(http_request.response)
+        }
+    }
+}
+
+
+function _elem(selector) {
+    return document.querySelector(selector)
+}
+LoadPageAuth () 
+/*Мoдуль авторизации*/
+function LoadPageAuth () {
+    _get ({url:'/modules/authorization.html'}, function(response) {
+        content.innerHTML = response;
+        OnloadAuth()
+    })
+}
+
+/*Кнопка авторизации*/ 
+function OnloadAuth() {
+    _elem ('.authorization').addEventListener('click', function (){
+        console.log('click')
+        let edata = new FormData()
+        let email = _elem('input[name="email"]').value
+        let password = _elem('input[name="password"]').value
+        edata.append('email',email)
+        edata.append('password',password)
+        
+        _post({url: `${host}/authorization`, data: edata}, function(response) {
+            response = JSON.parse(response)
+            console.log(response);
+            if (response.success) {
+                token = response.token
+                console.log(token)
+                LoadPageData ();
+                
+            } else {
+                alert("login failed")
+            }
+        })
+    })   
+}
+
+function LoadPageData() {
+let rdata = new FormData();
+rdata.append('token',token)
+let xhr = new XMLHttpRequest();
+xhr.open('POST', `${host}/data`);
+xhr.send(rdata);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        console.log(this.responseText)
+        if (xhr.status == 200) {
+            //document.body.innerHTML = xhr.responseText
+            let response = JSON.parse(xhr.responseText)
+            MakeTableData(response)
+        }
+        if (xhr.status == 401) {
+            let response = JSON.parse(xhr.responseText)
+            alert(response.message)
+        }
+    }
+}
+} 
+
+function LoadPageData (){
+    _get ({url:'data.html'}, function(response) {
+        content.innerHTML = response;
+        _elem('.exit').addEventListener('click', function (){
+            LoadPageAuth ()
+         })
+    })
+
+    let rdata = new FormData();
+    rdata.append('token', token)
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `${host}/data`);
+    xhr.send(rdata);
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        console.log(this.responseText)
+        if (xhr.status == 200) {
+            //document.body.innerHTML = xhr.responseText
+            let response = JSON.parse(xhr.responseText)
+            MakeTableData(response)
+        }
+        if (xhr.status == 401) {
+            let response = JSON.parse(xhr.responseText)
+            alert(response.message)
+        }
+    }
+}
+} 
+
+   /*Таблица данных*/
+function MakeTableCell(content) {
+    let cell = document.createElement('td')
+    cell.textContent = content;
+    return cell;
+}
+
+
+function MakeTableData (data) {
+    data.forEach(element => {
+       let row = document.createElement('tr')
+       row.append(MakeTableCell(element.name))
+       row.append(MakeTableCell(element.gender))
+       row.append(MakeTableCell(element.age))
+       row.append(MakeTableCell(element.eyeColor))
+       row.append(MakeTableCell(element.balance))
+       row.append(MakeTableCell(element.company))
+       row.append(MakeTableCell(element.email))
+       row.append(MakeTableCell(element.phone))
+       row.append(MakeTableCell(element.address))
+       row.append(MakeTableCell(element.favoriteFruit))
+    
+       document.querySelector('.user-files tbody').append(row)
+    });
+} 
+
+
+
+    /*function OnClickLogout() { 
+        //Кнопка выход
+        _elem ('.data head button').addEventListener('click', function (){
+    let rdata = new FormData();
+    rdata.append('token',token)
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `${host}/logout`);
+    xhr.send(rdata);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            console.log(this.responseText)
+           // content.innerHTML = xhr.responseText
+            if (xhr.status == 200) {
+                //document.body.innerHTML = xhr.responseText
+            }
+            if (xhr.status == 403) {
+                let response = JSON.parse(xhr.responseText)
+                alert(response.message)
+            }
+        }
+    }
+})
+}*/
